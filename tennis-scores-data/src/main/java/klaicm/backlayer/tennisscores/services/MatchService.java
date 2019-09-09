39e38f885b2aa33db56ea50c763a7aada8e125ef
klaicm <martin.klaic@gmail.com>
2019-09-09 22:15:32 +0200

    Refactored Services

--- tennis-scores-data/src/main/java/klaicm/backlayer/tennisscores/services/MatchService.java
+++ tennis-scores-data/src/main/java/klaicm/backlayer/tennisscores/services/MatchService.java
@@ -4,5 +4,4 @@ import klaicm.backlayer.tennisscores.model.Match;
 
 public interface MatchService extends CrudService<Match, Long> {
 
-    String test();
 }
--- tennis-scores-data/src/main/java/klaicm/backlayer/tennisscores/services/PlayerService.java
+++ tennis-scores-data/src/main/java/klaicm/backlayer/tennisscores/services/PlayerService.java
@@ -3,6 +3,4 @@ package klaicm.backlayer.tennisscores.services;
 import klaicm.backlayer.tennisscores.model.Player;
 
 public interface PlayerService extends CrudService<Player, Long> {
-
-    Player findByLastName(String lastName);
 }
